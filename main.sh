f9b22b4e342acc62ec878d24debe9a012a7d2197
Neris <mendozaene@gmail.com>
2016-02-29 02:17:59 -0600

    Configure dinamic hook

--- main.sh
+++ main.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # Obtain the host IP on OS X
 HOSTIP=$(/sbin/ifconfig en0 | grep 'inet' | cut -d: -f2 | awk '{ print $2}' | tr -d '\n')
+DIR_TEMPLATES="../templates"
 DIR_REPOS="."
 DIR_DEPLOYS="./deploys"
 printf "New project name: "
 read PROJECT_NAME
-echo "Live server ssh key required"
-printf "Live server [git@server:/dir/to/deploy]: "
+printf "Live server (Ex. git@server:/dir/to/deploy): "
 read LIVE_SERVER
 if [ -z $PROJECT_NAME ]; then 
-	printf "The project name is required!\n"
+	printf "The project name and live server are required!\n"
 else
 	if [ -d $DIR_REPOS/$PROJECT_NAME.git -o -d $DIR_DEPLOYS/$PROJECT_NAME ]; then
 		printf "A project with this name already exist!"
@@ -17,9 +17,10 @@ else
 		mkdir -p $DIR_DEPLOYS/$PROJECT_NAME
 		mkdir $DIR_REPOS/$PROJECT_NAME.git && cd $DIR_REPOS/$PROJECT_NAME.git
 		git init --bare
-		# Get hook template from git repo
-		touch hooks/post-receive
-		chmod -x hooks/post-receive
+		cp ../$DIR_TEMPLATES/post-receive ./hooks/
+		sed -i '' 's/deploy-test/'$PROJECT_NAME'/g' ./hooks/post-receive
+		sed -i '' 's~path_ssh~'$LIVE_SERVER'~g' ./hooks/post-receive
+		sed -i '' 's~path_tmp_source~'$DIR_DEPLOYS'/'$PROJECT_NAME'~g' ./hooks/post-receive
 		printf "Project created!\n"
 		printf "Get source on git@$HOSTIP:$DIR_REPOS/$PROJECT_NAME.git\n"
 	fi	
